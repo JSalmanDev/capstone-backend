923ea5f8130626f33632a3b0a20ebb64988e8680
JSalmanDev <jamil@isalman.net>
2020-12-18 14:49:06 +0500

    models relations & connected to db

--- server.js
+++ server.js
@@ -15,7 +15,43 @@ app.use(express.static(path.join(__dirname, 'public', 'uploads')));
 
 require("./routes/routes.js")(app);
 
+//DB Models
+const users = require("./modules/users/users_model");
+const categories = require("./modules/categories/categories_model");
+const questions = require("./modules/questions/questions_model");
+const answers = require("./modules/answers/answers_model") 
 
+let promiseArray = [];
+// // sync db models
+promiseArray.push(users.sync());
+promiseArray.push(categories.sync());
+
+Promise.all(promiseArray)
+  .then(() => {
+    questions.belongsTo(users);
+    users.hasMany(questions);
+    questions.belongsTo(categories);
+    categories.hasMany(questions);
+    questions.sync()
+      .then(() => {
+        answers.belongsTo(questions);
+        questions.hasMany(answers);
+        answers.belongsTo(users);
+        answers.sync()
+          .then(() => {
+            console.log('Tables synced successfully');
+          })
+          .catch(err => {
+            console.log("Table Sync Error Answers: ", err);
+          })
+      })
+      .catch(err => {
+        console.log("Table Sync Error Questions: ", err);
+      });
+  })
+  .catch(err => {
+    console.log("Table Sync Error: ", err);
+  });
 
 const PORT = process.env.PORT || 8081;
 
