5985528aca31d72673809149ef571fbbba58f1f4
JSalmanDev <jamil@isalman.net>
2020-12-18 14:54:30 +0500

    soft delete answer api func

--- modules/answers/answers_controller.js
+++ modules/answers/answers_controller.js
@@ -29,6 +29,24 @@ class Answers {
         }
     }
     
+    deleteAnswer() {
+        return (req, res) => { 
+            const { answer_id } = req.body;
+            
+            if (!req.body || !answer_id) {
+                return res.status(400).send({ msg: 'Bad Request' });
+            }
+
+            return answerModel.update({ is_deleted: true }, { where: { id: answer_id } })
+                .then(result => {
+                    return res.status(200).send({ msg: 'Answer Deleted Successfully' });
+                })
+                .catch(err => {
+                    console.log('Error in deleting specific answer from db', err);
+                    return res.status(500).json({ msg: 'Internal Server Error', error: err });
+                });
+        }
+    }
 }
 
 module.exports = new Answers();
